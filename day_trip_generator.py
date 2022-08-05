7ddb626f20019823f72d3e07c3923e3f3456050b
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-08-05 14:11:25 -0400

    The lists for destinations, restaurants, mode of transportation, and entertainment have been created.

--- day_trip_generator.py
+++ day_trip_generator.py
@@ -1,1 +1,5 @@
-print("Hello World")
+list_of_destinations = ["St. Petersburg", "New York City", "Los Angeles", "Tuscon", "Moab"]
+list_of_restaurants = ["Bodega", "Peking Duck House", "Republique" , "Kingfisher", "Moab Brewery"]
+mode_of_transportation = ["Rental Car", "Uber", "Public Transportation", "Walking"]
+list_of_entertainment = ["Movie Theater", "Concert", "Hiking", "Museums", "Camping"]
+
