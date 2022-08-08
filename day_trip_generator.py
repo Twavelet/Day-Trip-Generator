2e9ad4ddbaf41ee082c4e4c1b92f00f35edcb2b9
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-08-08 11:26:46 -0400

    added the ability for the program to recognize the words yes or no just in case the user inputs those words instead of y or no This feature was tested and functioning as intended.

--- day_trip_generator.py
+++ day_trip_generator.py
@@ -11,10 +11,10 @@ def destination_generator(destination_selector):
     print(random_destination[0])
     destination = input("Is this a destination you would like to visit? If so, type 'y' for 'Yes' or 'n' for 'No.' ").lower()
     while destination_selector:
-        if destination == "y":
+        if destination == "y" or destination == "yes":
             print(f"Perfect, {random_destination[0]} is where you're headed!")
             return random_destination[0]
-        elif destination == "n":
+        elif destination == "n" or destination == "no":
             random_destination = random.choices(destination_selector, k = 1)
             print(random_destination[0])
             destination = input("Does this destination sound more enticing?  If so, type 'y' for 'Yes' or 'n' for 'No.' ").lower()
@@ -27,10 +27,10 @@ def restaurant_generator(restaurant_selector):
     print(random_restaurant[0])
     restaurant = input("Does this restuarant sound appetizing to you? If so, type 'y' for 'Yes' or 'n' for 'No.' ").lower()
     while restaurant_selector:
-        if restaurant == "y":
+        if restaurant == "y" or restaurant == "yes":
             print(f"Perfect, {random_restaurant[0]} is where you'll be dining!")
             return random_restaurant[0]
-        elif restaurant == "n":
+        elif restaurant == "n" or restaurant == "no":
             random_restaurant = random.choices(restaurant_selector, k = 1)
             print(random_restaurant[0])
             restaurant = input("Does this restaurant seem more appealing?  If so, type 'y' for 'Yes' or 'n' for 'No.' ").lower()
@@ -43,10 +43,10 @@ def transportation_generator(transportation_selector):
     print(random_transportation[0])
     transportation = input("Is this how you'd prefer to get around town? If so, type 'y' for 'Yes' or 'n' for 'No.' ").lower()
     while transportation_selector:
-        if transportation == "y":
+        if transportation == "y" or transportation == "yes":
             print(f"Perfect, {random_transportation[0]} is how you'll be getting around town!")
             return random_transportation[0]
-        elif transportation == "n":
+        elif transportation == "n" or transportation == "no":
             random_transportation = random.choices(transportation_selector, k = 1)
             print(random_transportation[0])
             transportation = input("Does this mode of transportation sound better?  If so, type 'y' for 'Yes' or 'n' for 'No.' ").lower()
@@ -59,10 +59,10 @@ def entertainment_generator(entertainment_selector):
     print(random_entertainment[0])
     entertainment = input("Does this sound like something you'd enjoy doing? If so, type 'y' for 'Yes' or 'n' for 'No.' ").lower()
     while entertainment_selector:
-        if entertainment == "y":
+        if entertainment == "y" or entertainment == "yes":
             print(f"Perfect, {random_entertainment[0]} is how you'll be spending your time while you're there!")
             return random_entertainment[0]
-        elif entertainment == "n":
+        elif entertainment == "n" or entertainment == "no":
             random_entertainment = random.choices(entertainment_selector, k = 1)
             print(random_entertainment[0])
             entertainment = input("Does this seem like an activity you'd enjoy more?  If so, type 'y' for 'Yes' or 'n' for 'No.' ").lower()
@@ -77,9 +77,9 @@ verified_entertainment = entertainment_generator(form_of_entertainment)
 def itinerary_generator():
     print(f"Awesome! Now that you have selected everything for your upcoming trip, here is your itinerary:\nDestination: {verified_destination}\nRestaurant: {verified_restaurant}\nTransportation: {verified_transportation}\nEntertainment: {verified_entertainment}")
     final_user_input = input("Does your itinerary suit your fancy? If so, type 'y' for 'Yes' or 'n' for 'No.' ").lower()
-    if final_user_input == "y":
+    if final_user_input == "y" or final_user_input == "yes":
         print("Awesome! All thats left to do now is pack and get on over there!")
-    elif final_user_input == "n":
+    elif final_user_input == "n" or final_user_input == "no":
         return final_generator()
 
 def final_generator():
